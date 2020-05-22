12cba5d1e41cd8fcecc98d42a1caffae4fa243d3
TSCNBLUE <41600228+TSCNBLUE@users.noreply.github.com>
2020-05-22 22:40:37 +0800

    update

---
+++ Leetcode/code/349-两个数组的交集.cpp
@@ -0,0 +1,16 @@
+#include<iostream> 
+#include<vector>
+#include<set>
+
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        set<int> record( nums1.begin(), nums1.end());
+        set<int> resultSet;
+        for(int i = 0; i < nums2.size(); i++){
+            if(record.find(nums2[i]) != record.end())
+                resultSet.insert(nums2[i]);
+        }
+        return vector<int>(resultSet.begin(), resultSet.end());
+    }
+};
